be07c352a9d2f312f27c358f98140a103e48805e
Gavrilov. E <Egor4it@yandex.ru>
2022-05-17 23:18:57 +0500

    zzz

--- phones/models.py
+++ phones/models.py
@@ -8,7 +8,7 @@ class Phone(models.Model):
     price = models.IntegerField(default=None)
     image = models.URLField(default=None)
     release_date = models.DateField(default=None)
-    lte_exist = models.BooleanField(default=None)
+    lte_exists = models.BooleanField(default=None)
     slug = models.SlugField(default=None)
 
     def __str__(self):
